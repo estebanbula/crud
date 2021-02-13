f6c65c162b2b08b295a0d9cda7c21642f8efdbeb
estebanbula <36938874+estebanbula@users.noreply.github.com>
2021-02-13 12:22:38 -0500

    Task list & edit form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,34 @@ function App() {
     <div className = 'container mt-5'>
       <h1>WeTask</h1>
       <hr/>
+      <div className = 'row'>
+        <div className = 'col-8'> 
+          <h4 className = "text-center">Task List</h4>
+          <ul className = "list-group">
+            <li className = "list-group-item">
+              <span className = "lead">Task name</span>  
+              <button className = "btn btn-danger btn-sm float-right mx-2">Delete</button>
+              <button className = "btn btn-info btn-sm float-right mx-2">Edit</button>
+              <button className = "btn btn-success btn-sm float-right">Success</button>
+            </li>
+          </ul>
+        </div>
+        <div className = 'col-4'>
+          <h4 className = "text-center">Edit Form</h4>
+          <form>
+            <input
+              type = "text"
+              className = "form-control mb-2"
+              placeholder = "Add a new task..."
+            />
+            <button
+            className = "btn btn-dark btn-block"
+            type = "submit">
+              Add
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
