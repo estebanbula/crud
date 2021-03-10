611dd2d978d4439a96defced1af65f622ebe17ea
estebanbula <36938874+estebanbula@users.noreply.github.com>
2021-03-09 21:09:49 -0500

    Trying dark mode 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { isEmpty, size } from 'lodash'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from './components/GlobalStyles'
+import { lightTheme, darkTheme } from './components/Themes'
 import shortid from 'shortid'
 import { addDocument, deleteDocument, getCollection, updateDocument } from './actions'
 
@@ -9,6 +12,11 @@ function App() {
   const [editMode, setEditMode] = useState(false)
   const [id, setId] = useState("")
   const [error, setError] = useState(null)
+
+  const [theme, setTheme] = useState("light")
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light")
+  }
 
   useEffect(() => {
     (async () => {
@@ -91,79 +99,87 @@ function App() {
   }
 
   return (  
-    <div className = "container mt-5">
-      <title>WeTask</title>
-      <h1>WeTask</h1>
-      <hr/>
-      <div className = "row">
-        <div className = "col-8"> 
-          <h4 className = "text-center">Task List</h4>
-          {
-            size(tasks) === 0 ? (
-              <li className = "list-group-item">Add some tasks to start</li>
-            ) : (
-            <ul className = "list-group">
-              {
-                tasks.map((task) => (
-                <li className = "list-group-item" key={task.id}>
-                  <span className = "lead">{task.name}</span>  
-                  <button 
-                    className = "btn btn-danger btn-sm float-right mx-1"
-                    onClick = {() => deleteTask(task.id)}>
-                    Delete
-                  </button>
-                  <button 
-                    className = "btn btn-info btn-sm float-right mx-1"
-                    onClick = {() => editTask(task)}>
-                    Edit
-                  </button>
-                  <button 
-                    className = "btn btn-success btn-sm float-right">
-                    Done
-                  </button>
-                </li>
-                ))
-              }       
-            </ul>
-            )
-          }
-          <hr/>
-        </div>
-{/*           <div className = "col-8">
-            <h4 className = "text-center">Completed Task</h4>
-            <ul className = "list-group">
-              {
-                tasks.map((task) => (
-                  <li className = "list-group-item" key={task.id}>
-                    <span className = "lead">{task.name}</span>
-                  <button className = "btn btn-info btn-sm float-right mx-1">Restore</button>
-                  </li>
-                ))
-              }
-            </ul>
-          </div> */}
-        <div className = "col-4">
-          <h4 className = "text-center">{ editMode ? "Edit task" : "Add task"}</h4>
-          <form onSubmit = { editMode ? saveTask : addTask}>
+    <ThemeProvider theme = { theme === "light" ? lightTheme : darkTheme }>
+      <>
+      <GlobalStyles/>
+      <div className = "container mt-5">
+        <title>WeTask</title>
+        <button 
+          className = "btn btn-outline-dark float-left"
+          onClick = {themeToggler}>Switch Theme</button>
+        <h1>WeTask</h1>
+        <hr/>
+        <div className = "row">
+          <div className = "col-8"> 
+            <h4 className = "text-center">Task List</h4>
             {
-              error && <span className = "text-danger">{error}</span>
+              size(tasks) === 0 ? (
+                <li className = "list-group-item">Add some tasks to start</li>
+              ) : (
+              <ul className = "list-group">
+                {
+                  tasks.map((task) => (
+                  <li className = "list-group-item" key={task.id}>
+                    <span className = "lead">{task.name}</span>  
+                    <button 
+                      className = "btn btn-danger btn-sm float-right mx-1"
+                      onClick = {() => deleteTask(task.id)}>
+                      Delete
+                    </button>
+                    <button 
+                      className = "btn btn-info btn-sm float-right mx-1"
+                      onClick = {() => editTask(task)}>
+                      Edit
+                    </button>
+                    <button 
+                      className = "btn btn-success btn-sm float-right">
+                      Done
+                    </button>
+                  </li>
+                  ))
+                }       
+              </ul>
+              )
             }
-            <input
-              type = "text"
-              className = "form-control mb-2"
-              placeholder = "Add a new task..."
-              onChange = {(text) => setTask(text.target.value)}
-              value = {task}
-            />
-            <button
-              className = { editMode ? "btn btn-success btn-block" : "btn btn-dark btn-block"}
-              type = "submit">
-                { editMode ? "Save" : "Add" }
-            </button>
-          </form>
+            <hr/>
+          </div>
+  {/*           <div className = "col-8">
+              <h4 className = "text-center">Completed Task</h4>
+              <ul className = "list-group">
+                {
+                  tasks.map((task) => (
+                    <li className = "list-group-item" key={task.id}>
+                      <span className = "lead">{task.name}</span>
+                    <button className = "btn btn-info btn-sm float-right mx-1">Restore</button>
+                    </li>
+                  ))
+                }
+              </ul>
+            </div> */}
+          <div className = "col-4">
+            <h4 className = "text-center">{ editMode ? "Edit task" : "Add task"}</h4>
+            <form onSubmit = { editMode ? saveTask : addTask}>
+              {
+                error && <span className = "text-danger">{error}</span>
+              }
+              <input
+                type = "text"
+                className = "form-control mb-2"
+                placeholder = "Add a new task..."
+                onChange = {(text) => setTask(text.target.value)}
+                value = {task}
+              />
+              <button
+                className = { editMode ? "btn btn-success btn-block" : "btn btn-dark btn-block"}
+                type = "submit">
+                  { editMode ? "Save" : "Add" }
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+      </>
+      </ThemeProvider>
   );
 }
 
